3e7e7481b5422bf94eb67fd764a058580893db3f
axetroy <troy450409405@gmail.com>
2017-09-30 22:48:22 +0800

    feat: 添加淘宝的支持

--- app/providers/taobao.js
+++ app/providers/taobao.js
@@ -10,7 +10,8 @@ class QQProvider extends Provider {
     super();
   }
   async resolve(phone) {
-    const URL = `https://passport.alibaba.com/member/reg/fast/fast_reg.htm?_regfrom=ALIYUN`;
+    const URL = `https://reg.taobao.com/member/reg/fill_mobile.htm`;
+
     const browser = await puppeteer.launch({
       headless: config.isProduction
     });
@@ -25,41 +26,36 @@ class QQProvider extends Provider {
 
     await page.deleteCookie();
 
-    const [$nick, $password, $rePassword, $mobile] = await Promise.all([
-      page.$('#nick'),
-      page.$('#password'),
-      page.$('#rePassword'),
-      page.$('#mobile')
+    const [$btn, $mobile, $slide] = await Promise.all([
+      page.$('#J_AgreementBtn'),
+      page.$('#J_Mobile'),
+      page.$('#nc_1_n1z')
     ]);
 
-    await $nick.click();
-    await page.type(`张三abc1154`, { delay: 100 });
-
-    let psw = Math.random() + '';
-    await $password.click();
-    await page.type(psw, { delay: 100 });
-
-    await $rePassword.click();
-    await page.type(psw, { delay: 100 });
+    await $btn.click();
 
     await $mobile.click();
     await page.type(phone + '', { delay: 100 });
 
     // 按下鼠标，拖动滚动条
-    await page.mouse.move(365, 380);
-    await page.mouse.click(365, 380);
+    await page.mouse.move(415, 325, { step: 10 });
+    await $slide.click(415, 325);
     await page.mouse.down({
       button: 'left'
     });
 
-    await page.mouse.move(670, 380, { steps: 1 });
+    await utils.sleep(1000);
+
+    await page.mouse.move(690, 325, { steps: 10 });
+
+    await utils.sleep(1000);
 
     await page.mouse.up({ button: 'left' });
     // 松开鼠标
 
     await utils.sleep(1000);
 
-    await page.mouse.click(438, 433);
+    await page.mouse.click(500, 380);
 
     await browser.close();
   }
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const App = require('./app/app');
 const QQProvider = require('./app/providers/qq');
 const AliyunProvider = require('./app/providers/aliyun');
+const TaobaoProvider = require('./app/providers/taobao');
 
 new App(13377175342)
   .provider(QQProvider)
   .provider(AliyunProvider)
+  .provider(TaobaoProvider)
   .bootstrap();
