d3d3a592e387f2f23447f644a39761e2c29782bf
axetroy <troy450409405@gmail.com>
2017-10-08 02:48:25 +0800

    update

--- app/providers/youku.js
+++ app/providers/youku.js
@@ -4,41 +4,29 @@ const utils = require('../utils');
 class QQProvider extends Provider {
   constructor() {
     super();
-    this.url = `http://www.youku.com/`;
+    this.url = `https://account.youku.com/register.htm`;
   }
   async resolve(ctx) {
     const options = ctx.options;
     const page = ctx.page;
 
-    const $user = await page.$('#qheader_login'); // 点击登陆
-
-    await $user.click({ button: 'left' });
-
-    await utils.sleep(2000);
-
-    const $go2register = await page.$('#YT-registeBtn'); // phone
-
-    await $go2register.click({ button: 'left' });
-
-    await utils.sleep(2000);
-
-    const [$phone, $password, $repassword, $send] = await Promise.all([
-      page.$('#YT-mPassport'), // phone
-      page.$('#YT-mRegPassword'), // password
-      page.$('#YT-mRepeatPwd'), // 确认密码
-      page.$('#YT-mGetMobileCode') // 发送验证码
+    const [$mobile, $password, $repassword, $submit] = await Promise.all([
+      page.$('#passport'),
+      page.$('#password'),
+      page.$('#repeatPsd'),
+      page.$('#getMobileCode')
     ]);
 
-    await $phone.click({ button: 'left' });
+    await $mobile.click();
     await page.type(options.phone, { delay: 100 });
 
-    await $password.click({ button: 'left' });
+    await $password.click();
     await page.type(options.password, { delay: 100 });
 
-    await $repassword.click({ button: 'left' });
+    await $repassword.click();
     await page.type(options.password, { delay: 100 });
 
-    await $send.click({ button: 'left' });
+    await $submit.click();
   }
 }
 
