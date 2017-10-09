77c8591855010d4cf344c5dd78b7ba209c3ed8a4
axetroy <troy450409405@gmail.com>
2017-10-10 01:01:47 +0800

    feat: 添加支付宝爆破登陆

---
+++ app/providers/支付宝-登陆.js
@@ -0,0 +1,47 @@
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `https://authgtj.alipay.com/login/index.htm`;
+    this.alone = true;
+    this.times = 0;
+  }
+  async resolve(ctx) {
+    this.times++;
+    const options = ctx.options;
+
+    const page = ctx.page;
+
+    await page.evaluate(() => {
+      const $username = document.querySelector('#J-input-user');
+      $username.value = '';
+    });
+
+    await utils.sleep(1000);
+
+    const [$mobile, $password, $login] = await Promise.all([
+      page.$('#J-input-user'),
+      page.$('#password_rsainput'),
+      page.$('#J-login-btn')
+    ]);
+
+    await $mobile.click();
+    await page.type(options.phone, { delay: 100 });
+
+    await $password.click();
+    await page.type(options.password, { delay: 100 });
+
+    await $login.click();
+
+    await utils.sleep(3000);
+
+    // 重试5次
+    if (this.times % 5 !== 0) {
+      try {
+        await this.resolve(ctx);
+      } catch (err) {}
+    }
+  }
+};
