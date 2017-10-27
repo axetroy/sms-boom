b6f2bc7329d91ade7276d2178caacbba32e98dd2
axetroy <troy450409405@gmail.com>
2017-10-28 01:03:03 +0800

    feat: 增加中关村在线provider

---
+++ app/providers/中关村在线.js
@@ -0,0 +1,22 @@
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `http://service.zol.com.cn/user/siteLogin.php`;
+}
+  async resolve(ctx) {
+    const options = ctx.options;
+    const page = ctx.page;
+
+    await page.type('#tel', options.phone, { delay: 50 });
+
+    await utils.sleep(500);
+
+    await page.click('#get-anicode');
+
+    // 检验是否发送成功
+    await page.waitForSelector('.resendphonecode', { timeout: 1000 * 3 });
+  }
+};
