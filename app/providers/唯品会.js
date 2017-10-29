fb8a5be91504f0b13373b0506e92f23faefb526e
axetroy <troy450409405@gmail.com>
2017-10-29 21:37:08 +0800

    feat: 添加唯品会provider

---
+++ app/providers/唯品会.js
@@ -0,0 +1,24 @@
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `https://passport.vip.com/register`;
+    this.alone = true;
+  }
+  async resolve(ctx) {
+    const options = ctx.options;
+    const page = ctx.page;
+
+    await page.type('#J_mobile_name', options.phone, { delay: 100 });
+    await page.type('#J_mobile_code', '1234', { delay: 100 });
+
+    await utils.sleep(500);
+
+    await page.click('#J_mobile_verifycode_btn');
+
+    // 检验是否发送成功
+    await page.waitForSelector('#J_mobile_verifycode_btn.ui-btn-disable', { timeout: 1000 * 3 });
+  }
+};
