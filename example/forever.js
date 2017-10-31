f2c0c207a107947d75d93e6acaac68e90904c3e7
axetroy <troy450409405@gmail.com>
2017-10-31 15:53:27 +0800

    docs: Update example

--- example/forever.js
+++ example/forever.js
@@ -1,13 +1,15 @@
+const chalk = require('chalk');
 const boomer = require('../index');
-
 const app = boomer(process.env.PHONE, { once: false });
 
 app
   .on('open', () => {
     console.info(`打开浏览器...`);
   })
-  .on('next', (ctx) => {
-    console.info(`进入到 ${ctx.currentPage}`);
+  .on('next', (currentTarget) => {
+    utils.info(
+      `进入到 ${chalk.green(currentTarget.name)} ${chalk.green.underline(currentTarget.url)}`
+    );
   })
   .on('error', err => {
     console.error(err);
