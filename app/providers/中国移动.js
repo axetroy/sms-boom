07e9a3688497adf00b5fed93c2f7fb2893505a42
axetroy <troy450409405@gmail.com>
2017-11-19 19:16:42 +0800

    feat: 添加中国移动provider

---
+++ app/providers/中国移动.js
@@ -0,0 +1,24 @@
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `https://www.cmpassport.com/umc/reg/alias/?from=9`;
+  }
+  async resolve(ctx) {
+    const options = ctx.options;
+    const page = ctx.page;
+
+    await utils.mockUserMouse(page);
+
+    await page.type('#txtPhone', options.phone, { delay: 50 });
+
+    await page.type('#txtSmsCode', options.phone, { delay: 50 });
+
+    await page.click('#btnGetSmsCode');
+
+    // 检验是否发送成功
+    await page.waitForSelector('.getSmsCode.BtnCode', { timeout: 1000 * 3 });
+  }
+};
