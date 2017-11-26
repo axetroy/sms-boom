7fc61d09cbef4c499e73bd3933262a390bd88015
axetroy <troy450409405@gmail.com>
2017-11-26 18:16:01 +0800

    feat: 增加手拉手商城provider

---
+++ app/providers/手拉手商城.js
@@ -0,0 +1,32 @@
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `http://mall.slswd.com/register!toPc.htm`;
+  }
+  async resolve(ctx) {
+    const options = ctx.options;
+    const page = ctx.page;
+
+    await utils.mockUserMouse(page);
+
+    await page.type('#userName', options.phone, { delay: 100 });
+
+    // 按下鼠标，拖动滚动条
+    await page.mouse.move(302, 369);
+    await page.mouse.down();
+    await page.mouse.move(552, 366, { steps: 20 });
+    await utils.sleep(500);
+    await page.mouse.up();
+    // 松开鼠标
+
+    await page.click('#button');
+
+    await utils.sleep(100);
+
+    // 检验是否发送成功
+    await page.waitForSelector('#button[disabled]');
+  }
+};
