61f71ea8c370401111b1d61c84cd47d5d7e302f7
axetroy <troy450409405@gmail.com>
2017-11-26 18:19:22 +0800

    feat: 添加折800provider

---
+++ app/providers/折800.js
@@ -0,0 +1,31 @@
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `https://passport.zhe800.com/users/signup`;
+  }
+  async resolve(ctx) {
+    const options = ctx.options;
+    const page = ctx.page;
+
+    await utils.mockUserMouse(page);
+
+    await page.type('#pemail', options.phone, { delay: 100 });
+
+    // 按下鼠标，拖动滚动条
+    await page.mouse.move(340, 355);
+    await page.mouse.down();
+    await page.mouse.move(630, 350, { steps: 20 });
+    await utils.sleep(500);
+    await page.mouse.up();
+    // 松开鼠标
+
+    await page.click('#i_codeP a');
+
+    await utils.sleep(100);
+
+    // 检验是否发送成功
+  }
+};
