ccf0aee31a1979e5d4ec744c026431d63cf4cecd
WindomZ <git.windomz@gmail.com>
2017-12-02 22:29:36 +0800

    feat: add 微盟 provider

--- app/providers/微盟.js
+++ app/providers/微盟.js
@@ -6,19 +6,19 @@ const utils = require('../utils');
 module.exports = class extends Provider {
   constructor() {
     super();
-    this.url = `http://reg.ztgame.com/`;
+    this.url = `https://account.weimob.com/register`;
   }
 
   async resolve(ctx) {
     const options = ctx.options;
     const page = ctx.page;
 
-    await page.type('#reg_form input[name=phone]', options.phone, { delay: 100 });
+    await page.type('input#phone', options.phone, { delay: 100 });
 
-    await page.click('#reg_form input.get-mpcode');
+    await page.click('a.getcode');
 
     //    // 检验是否发送成功
-    //    await page.waitForSelector('#reg_form input.get-mpcode[disabled]', {
+    //    await page.waitForSelector('a.getcode.disabled', {
     //      timeout: 1000 * 3,
     //    });
   }
