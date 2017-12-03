0bd61bdd8898990df097b6eb664b742d263f7343
axetroy <troy450409405@gmail.com>
2017-12-03 22:32:42 +0800

    fix: 企查查provider没有正确检测是否发送短信成功

--- app/providers/企查查.js
+++ app/providers/企查查.js
@@ -7,16 +7,18 @@ module.exports = class extends Provider {
   constructor() {
     super();
     this.url = `http://www.qichacha.com/user_register`;
+    this.alone = true;
   }
 
   async resolve(ctx) {
     const options = ctx.options;
     const page = ctx.page;
 
-    await page.type('input[name=phone]', options.phone, { delay: 100 });
+    await page.type('input[name=phone]', options.phone, { delay: 50 });
 
     await page.type('input[name=pswd]', options.password, { delay: 50 });
 
+    await utils.mockUserMouse(page);
     await page.mouse.move(841, 345);
     await page.mouse.down();
     await page.mouse.move(1101, 346, { steps: 20 });
@@ -24,6 +26,13 @@ module.exports = class extends Provider {
 
     await page.click('a.get-mobile-code');
 
-    //    await page.waitForSelector('span.input-group-btn button:first-child[disabled]', { timeout: 1000 * 3 });
+    const isSuccess = await page.evaluate(() => {
+      const btn = document.querySelector('a.get-mobile-code');
+      return btn.innerText.indexOf('重新发送') >= 0;
+    });
+
+    if (!isSuccess) {
+      throw null;
+    }
   }
 };
