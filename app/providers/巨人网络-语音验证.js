5462e4b523950957d0537c096de8ba5ecb4c448d
axetroy <troy450409405@gmail.com>
2017-12-03 20:42:32 +0800

    feat: 增加巨人网络的语言验证码

---
+++ app/providers/巨人网络-语音验证.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `http://reg.ztgame.com/`;
+  }
+
+  async resolve(ctx) {
+    const options = ctx.options;
+    const page = ctx.page;
+
+    await page.type('#reg_form input[name=phone]', options.phone, { delay: 50 });
+
+    // 获取语言验证码
+    await page.click('#reg_form input.voice_mpcode');
+
+    // 检验是否发生成功
+    try {
+      await page.waitForSelector('.get-mpcode[disabled]', { timeout: 1000 * 3 });
+    } catch (err) {
+      throw null;
+    }
+  }
+};
