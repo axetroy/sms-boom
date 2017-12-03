7e36751c40c46d4193b4b91af8418b7e87c78fe5
axetroy <troy450409405@gmail.com>
2017-12-03 20:51:56 +0800

    feat: 添加乾贷网的电话验证码

---
+++ app/providers/乾贷网语言.js
@@ -0,0 +1,37 @@
+const Provider = require('../provider');
+const utils = require('../utils');
+
+module.exports = class extends Provider {
+  constructor() {
+    super();
+    this.url = `https://www.qiandw.com/Account/RegisterNew`;
+  }
+  async resolve(ctx) {
+    const options = ctx.options;
+    const page = ctx.page;
+
+    await page.type('#mobile', options.phone, { delay: 50 });
+
+    // 选择电话验证码
+    await page.click('[name="voice"]');
+
+    await utils.mockUserMouse(page);
+    // 按下鼠标，拖动滚动条
+    await page.mouse.move(510, 344);
+    await page.mouse.down();
+    await page.mouse.move(860, 338, { steps: 5 });
+    await page.mouse.up();
+    // 松开鼠标
+
+    // 检验是否发送成功
+    try {
+      await page.evaluate(() => {
+        const display = document.querySelector('.resend.text-send').style.display;
+        // 倒计时应该会被显示出来
+        return display !== 'none' && display !== '';
+      });
+    } catch (err) {
+      throw null;
+    }
+  }
+};
