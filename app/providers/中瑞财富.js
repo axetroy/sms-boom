4e6c7360b52097a5e5cf5455ed5a36402a8887a7
axetroy <troy450409405@gmail.com>
2017-12-03 20:53:02 +0800

    fix: 修复中瑞财富验证是否发送成功不正确的问题

--- app/providers/中瑞财富.js
+++ app/providers/中瑞财富.js
@@ -13,15 +13,18 @@ module.exports = class extends Provider {
     const options = ctx.options;
     const page = ctx.page;
 
-    await page.type('input.accname_input', options.phone, {delay: 100});
+    await page.type('input.accname_input', options.phone, { delay: 100 });
 
-    await page.type('input[name=password]', options.password, {delay: 50});
-    await page.type('input[name=password1]', options.password, {delay: 50});
+    await page.type('input[name=password]', options.password, { delay: 50 });
+    await page.type('input[name=password1]', options.password, { delay: 50 });
 
     await page.click('input#sendsms-for-regiter');
 
     // TODO: 可能会有验证码
-
-    await page.waitForSelector('input#sendsms-for-regiter[disabled]', {timeout: 1000 * 3});
+    try {
+      await page.waitForSelector('input#sendsms-for-regiter[disabled]', { timeout: 1000 * 3 });
+    } catch (err) {
+      throw null;
+    }
   }
 };
